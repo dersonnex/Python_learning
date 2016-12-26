01b1a685a050269cfb81f7d2a3a483d38de5cac5
Sonne <dersonnex@gmail.com>
2016-12-27 00:37:28 +0800

    Add files via upload

---
+++ Learn_PhythonEx/ex10.py
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'M tabbed in."
+persian_cat = "I'm splt\non a line."
+bakslash_cat = "I'm \\ a \\ cat."
+
+# spaceing is very important in this ex. 
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print bakslash_cat
+print fat_cat

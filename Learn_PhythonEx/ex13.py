fa01af5fa50614711db823668f64bed077e4cd6e
Sonne <dersonnex@gmail.com>
2016-12-27 01:54:59 +0800

    Delete ex13.py

--- Learn_PhythonEx/ex13.py
+++
@@ -1,8 +0,0 @@
-from sys import argv #import, add features 
-
-script, first, second, third = argv
-
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
